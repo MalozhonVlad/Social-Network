a26162109cc2e0a02defdaedf721409960b602dc
Vlad <malozhonvlad@gmail.com>
2020-07-11 17:29:00 +0200

    11 video 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://lh3.googleusercontent.com/proxy/cSdxnuAWLCXr35YozZmy3pRuFqLa2mSAgb3ljzatZ8t5H53FpV4TQbtgm7pf-xD5wt1p6hXrIPXKaybv_YI1T8YL8t5svgMx3ovUS7Vu1pMXgNGyyv6_Nw'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src='https://www.robertwalters.com.hk/content/dam/robert-walters/global/images/article-images/digital-neon.jpg'/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
