f91b39265558051084de208744858febb7f7fe17
Vlad <malozhonvlad@gmail.com>
2020-07-12 09:50:22 +0200

    12 video

---
+++ src/compomemts/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://lh3.googleusercontent.com/proxy/cSdxnuAWLCXr35YozZmy3pRuFqLa2mSAgb3ljzatZ8t5H53FpV4TQbtgm7pf-xD5wt1p6hXrIPXKaybv_YI1T8YL8t5svgMx3ovUS7Vu1pMXgNGyyv6_Nw'/>
+        </header>
+    );
+}
+
+export default Header;
