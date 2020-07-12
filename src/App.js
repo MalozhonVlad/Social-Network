ba571097a5b3769102d23239c39a68033f6e5c67
Vlad <malozhonvlad@gmail.com>
2020-07-13 00:43:28 +0200

    19 video

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Header from "./compomemts/header/Header.jsx";
 import Navbar from "./compomemts/navbar/Navbar";
 import Dialogs from "./compomemts/dialogs/Dialogs";
 import Profile from "./compomemts/profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./compomemts/news/News";
+import Music from "./compomemts/music/Music";
+import Settings from "./compomemts/settings/Settings";
 
-function App(props) {
+
+const App = (props) => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                {/*<Dialogs/>*/}
-                <Profile />
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
