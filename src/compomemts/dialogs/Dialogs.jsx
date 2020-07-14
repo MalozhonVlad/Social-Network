9820b2c026d227ee2621580439457588ca4aff4f
Vlad <malozhonvlad@gmail.com>
2020-07-14 23:47:38 +0200

    21 video

--- src/compomemts/dialogs/Dialogs.jsx
+++ src/compomemts/dialogs/Dialogs.jsx
@@ -3,9 +3,27 @@ import style from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-      <div>
-          Dialogs
-      </div>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Vlad
+                </div>
+                <div className={style.dialog}>
+                    Andrey
+                </div>
+                <div className={style.dialog}>
+                    Sergey
+                </div>
+                <div className={style.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How is your name</div>
+                <div className={style.message}>Yo</div>
+            </div>
+        </div>
     );
 }
 
