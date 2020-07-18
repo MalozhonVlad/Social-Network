53e16834ee5cb45464a111c5b96e720d651c3d64
Vlad <malozhonvlad@gmail.com>
2020-07-18 12:48:03 +0200

    24 video

--- src/compomemts/dialogs/Dialogs.jsx
+++ src/compomemts/dialogs/Dialogs.jsx
@@ -6,7 +6,7 @@ const DialogItem = (props) => {
 
     let path = "/dialogs/" + props.id;
 
-    return(
+    return (
         <div className={style.dialog + ' ' + style.active}>
             <NavLink to={path}>{props.name}</NavLink>
         </div>
@@ -20,17 +20,35 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Vlad'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sergey'},
+        {id: 4, name: 'Viktor'},
+        {id: 5, name: 'Valentin'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your name'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'}
+    ]
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <DialogItem name="Vlad" id="1"/>
-                <DialogItem name="Andrey" id="2"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
                 <DialogItem name="Sergey" id="3"/>
                 <DialogItem name="Viktor" id="4"/>
             </div>
             <div className={style.messages}>
-                <Message message="Hi"/>
-                <Message message="How is your name"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message="Yo"/>
+                <Message message="Yo"/>
                 <Message message="Yo"/>
             </div>
         </div>
--- src/compomemts/profile/myPosts/MyPosts.jsx
+++ src/compomemts/profile/myPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import MyPost from "./post/MyPost";
 import style from "./MyPosts.module.css"
 
 const MyPosts = (props) => {
+
+    let postsData = [
+        {id:1, message: 'Hi, how are you', likesCount: 10},
+        {id:2, message: 'My first post', likesCount: 15}
+    ]
+
     return (
         <div className={style.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +21,8 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={style.posts}>
-                <MyPost message='Hi, how are you' likesCount='10'/>
-                <MyPost message='My first post' likesCount='15'/>
+                <MyPost message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <MyPost message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </div>
     );
