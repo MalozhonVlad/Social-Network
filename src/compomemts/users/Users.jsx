4fbf53829714de66bac1750334aa86ef1e9cc543
Vlad <malozhonvlad@gmail.com>
2020-08-02 01:23:59 +0200

    56 video

--- src/compomemts/users/Users.jsx
+++ src/compomemts/users/Users.jsx
@@ -1,49 +1,28 @@
 import React from "react";
 import styles from "./users.module.css";
-import userPhoto from '../../assets/images/user.png'
-import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    /**
-     * жизнений цикл компоненти !!! типа как в спринге жизнений цикл бина !!!
-     * етот метод визивается когда создается етот обьект и в ето время идет запрос на сервер !!!
-     */
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            });
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            });
-    }
+    return <div>
+        <div>
+            {pages.map(page => {
+                return <span className={props.currentPage === page && styles.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(page)
+                             }}>{page}</span>
+            })}
 
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return <div>
-            <div>
-                {pages.map(page => {
-                   return <span className={this.props.currentPage === page && styles.selectedPage}
-                   onClick={(e) => { this.onPageChanged(page) }}>{page}</span>
-                })}
-
-            </div>
-            {
-                this.props.users.map(user => <div key={user.id}>
+        </div>
+        {
+            props.users.map(user => <div key={user.id}>
                     <span>
                         <div>
                             <img src={user.photos.small != null ? user.photos.small : userPhoto}
@@ -52,14 +31,14 @@ class Users extends React.Component {
                         <div>
                             {user.followed
                                 ? <button onClick={() => {
-                                    this.props.unfollow(user.id)
+                                    props.unfollow(user.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    this.props.follow(user.id)
+                                    props.follow(user.id)
                                 }}>Follow</button>}
                         </div>
                     </span>
-                    <span>
+                <span>
                         <span>
                             <div>{user.name}</div>
                             <div>{user.status}</div>
@@ -69,10 +48,9 @@ class Users extends React.Component {
                             <div>{"user.location.city"}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    }
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
--- src/compomemts/users/UsersContainer.jsx
+++ src/compomemts/users/UsersContainer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
+import * as axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+
+    /**
+     * жизнений цикл компоненти !!! типа как в спринге жизнений цикл бина !!!
+     * етот метод визивается когда создается етот обьект и в ето время идет запрос на сервер !!!
+     */
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}/>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -45,4 +79,4 @@ let mapDispatchToProps = (dispatch) => {
  * обсервер которий говорит данние в стейте поменялись и потом визивает функцию mapStateToProps -
  * которая заново отрисовивает компоненту !!!!!
  */
-export default connect(mapStateToProps,mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
