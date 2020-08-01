e52c3526077cc94a25f3265b07c02662acd6ffc5
Vlad <malozhonvlad@gmail.com>
2020-08-01 13:55:25 +0200

    51 video

--- src/compomemts/users/Users.jsx
+++ src/compomemts/users/Users.jsx
@@ -1,43 +1,15 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://i.pinimg.com/236x/8a/05/33/8a053319bd263f33acaccd922f55e451.jpg',
-                followed: false,
-                fullName: 'Dmitriy',
-                status: 'I am a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://i.pinimg.com/236x/8a/05/33/8a053319bd263f33acaccd922f55e451.jpg',
-                followed: true,
-                fullName: 'Vlad',
-                status: 'I am a boss too',
-                location: {city: 'Moscov', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://i.pinimg.com/236x/8a/05/33/8a053319bd263f33acaccd922f55e451.jpg',
-                followed: false,
-                fullName: 'Andrey',
-                status: 'I am a boss too',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://i.pinimg.com/236x/8a/05/33/8a053319bd263f33acaccd922f55e451.jpg',
-                followed: true,
-                fullName: 'Valera',
-                status: 'From USA',
-                location: {city: 'New York', country: 'USA'}
-            }
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
     }
 
     return (
@@ -46,7 +18,8 @@ let Users = (props) => {
                 props.users.map(user => <div key={user.id}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} className={styles.userPhoto}/>
+                            <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                                 className={styles.userPhoto}/>
                         </div>
                         <div>
                             {user.followed
@@ -60,12 +33,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
                         <span>
-                            <div>{user.location.country}</div>
-                            <div>{user.location.city}</div>
+                            <div>{"user.location.country"}</div>
+                            <div>{"user.location.city"}</div>
                         </span>
                     </span>
                 </div>)
