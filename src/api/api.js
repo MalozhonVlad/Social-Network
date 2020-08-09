61f54022a7fc135229a1c586caa5317b75e70a5a
Vlad <malozhonvlad@gmail.com>
2020-08-09 13:28:32 +0200

    63 video

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "api-key": "2a515b19-5a52-4bab-a3fc-034b49d5cb8c"
+    }
+});
+
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            });
+    }
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        })
+}
